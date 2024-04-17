2a430223ed6231a9a230c0b7afc4ddaac728b5d4
ujnss <unlinunlin@163.com>
2024-04-17 08:26:16 +0000

    define ffi

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use std::ffi::CString;
+use std::os::raw::c_char;
+
+#[no_mangle]
+#[export_name = "keygen"]
+pub extern "C" fn __keygen(_c_param: *const c_char) -> *const c_char {
+    CString::new("{}".to_string()).unwrap().into_raw()
+}
+#[no_mangle]
+#[export_name = "encrypt"]
+pub extern "C" fn __encrypt(_c_param: *const c_char) -> *const c_char {
+    CString::new("{}".to_string()).unwrap().into_raw()
+}
+
+#[no_mangle]
+#[export_name = "reencrypt"]
+pub extern "C" fn __reencrypt(_c_param: *const c_char) -> *const c_char {
+    CString::new("{}".to_string()).unwrap().into_raw()
+}
+
+#[no_mangle]
+#[export_name = "decrypt"]
+pub extern "C" fn __decrypt(_c_param: *const c_char) -> *const c_char {
+    CString::new("{}".to_string()).unwrap().into_raw()
+}
+
 pub fn main() {
     println!("initialize ok");
 }
