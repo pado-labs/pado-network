fa40be0d358ba5f99a2939277f6ed0f3162e1f2a
ujnss <unlinunlin@163.com>
2024-08-12 02:59:54 +0000

    docker build

--- padonode/docker-build.sh
+++ padonode/docker-build.sh
@@ -1,8 +1,11 @@
 #！/bin/bash
-if ! type node > /dev/null 2>&1; then
+if ! type node >/dev/null 2>&1; then
   echo "Please install nodejs with version > 18"
   exit 1
 fi
+
 npm install
 npm run build
-docker build -t padolabs/pado-network:latest .
+
+docker build -t padolabs/pado-network:latest . -f Dockerfile.build
+exit 0
