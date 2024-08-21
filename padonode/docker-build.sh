fcce3121ad4e9266196b421c643fa20b37f8ffd7
ujnss <unlinunlin@163.com>
2024-08-21 06:27:53 +0000

    Bump version to 0.2.0

--- padonode/docker-build.sh
+++ padonode/docker-build.sh
@@ -7,6 +7,12 @@ fi
 npm install
 npm run build
 
+ver=$(cat package.json | grep version | grep -Eo "[0-9]+\.[0-9]+\.[0-9]+")
+if [ $ver ]; then
+  tag=v${ver}
+  docker build -t padolabs/pado-network:${tag} . -f Dockerfile.build
+fi
+
 docker build -t padolabs/pado-network:latest . -f Dockerfile.build
 # docker build --progress=plain --no-cache -t padolabs/pado-network:latest . -f Dockerfile.build
 exit 0
